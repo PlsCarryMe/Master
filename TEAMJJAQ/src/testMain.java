c068bfac5b3c2408adbba1f6a291b6f623799282
Anthony Trang <Tranganthony97@gmail.com>
2019-02-06 19:25:52 -0800

    One more test push after a merge of master and a local branch

--- TEAMJJAQ/src/testMain.java
+++ TEAMJJAQ/src/testMain.java
@@ -3,7 +3,8 @@ public class testMain {
 
 	public static void main(String[] args) {
 		System.out.println("Hello world!");
-		System.out.println("I'm on the master branch in my eclipse");	
+		System.out.println("I'm on the master branch in my eclipse");
+		//This line below me was after merge that also had a conflict.
 		System.out.println("I'm on the second branch in my eclipse");
 
 	}
