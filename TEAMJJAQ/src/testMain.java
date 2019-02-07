2410cfb89a44dc4d6db0011686f896298e164a78
Anthony Trang <Tranganthony97@gmail.com>
2019-02-06 19:18:58 -0800

    Added line on the 6th line to test merge with second

--- TEAMJJAQ/src/testMain.java
+++ TEAMJJAQ/src/testMain.java
@@ -3,7 +3,7 @@ public class testMain {
 
 	public static void main(String[] args) {
 		System.out.println("Hello world!");
-		System.out.println("This is something I added after my first failed attempt");
+		System.out.println("I'm on the master branch in my eclipse");
 
 	}
 
